bbd68c0da90648d10c7f52b2710d1a097f45c150
Cody Nicholson <codynicholson96@gmail.com>
2020-09-23 22:27:10 -0500

    Added postConstruct actual data

--- src/main/java/com/projecttrackerapi/dao/ProjectDao.java
+++ src/main/java/com/projecttrackerapi/dao/ProjectDao.java
@@ -49,7 +49,7 @@ public class ProjectDao {
     }
 
     @Transactional
-    public DeleteProjectResponseModel deleteProject(UUID id){
+    public DeleteProjectResponseModel deleteProject(UUID id) {
         Project project = projectRepository.getById(id);
 
         if (project == null) {
@@ -66,7 +66,7 @@ public class ProjectDao {
         return new DeleteProjectResponseModel(deletedProjectDto, deletedProjectTaskDtos);
     }
 
-    public ProjectTaskDto saveOrUpdateProjectTask(ProjectTaskDto projectTaskDto){
+    public ProjectTaskDto saveOrUpdateProjectTask(ProjectTaskDto projectTaskDto) {
         logger.info(Constants.saveOrUpdateProjectTaskMessage(projectTaskDto));
         ProjectTask savedProjectTask = projectTaskRepository.save(projectTaskDtoToEntity(projectTaskDto));
         return projectTaskEntityToDto(savedProjectTask);
@@ -79,11 +79,11 @@ public class ProjectDao {
         return projectTaskEntitiesToDtos(projectTasksList);
     }
 
-    public ProjectTaskDto findProjectTaskById(UUID id){
+    public ProjectTaskDto findProjectTaskById(UUID id) {
         return projectTaskEntityToDto(projectTaskRepository.getById(id));
     }
 
-    public ProjectTaskDto deleteProjectTaskById(UUID id){
+    public ProjectTaskDto deleteProjectTaskById(UUID id) {
         ProjectTask projectTask = projectTaskRepository.getById(id);
         projectTaskRepository.delete(projectTask);
         ProjectTaskDto deletedProjectTaskDto = projectTaskEntityToDto(projectTask);
@@ -131,18 +131,49 @@ public class ProjectDao {
 
     @PostConstruct
     @Transactional
-    private void fillTestData(){
-        Project project1 = new Project(UUID.randomUUID(), "project1", "description1", new Date(), null, "deployedLink1", "docLink1", "codeLink1");
-        Project project2 = new Project(UUID.randomUUID(), "project2", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+    private void fillTestData() {
+        Project projectTrackingProject = new Project(UUID.randomUUID(), "Project Tracking App", "An application I created to track all of my side projects", new Date(), null, "deployedLink1", "docLink1", "codeLink1");
+        Project issTrackingProject = new Project(UUID.randomUUID(), "ISS Tracking App", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project battleApp = new Project(UUID.randomUUID(), "Battle App", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project projectDesignerProject = new Project(UUID.randomUUID(), "Project Designer App", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project tronApp = new Project(UUID.randomUUID(), "Tron App", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project bloggingApp = new Project(UUID.randomUUID(), "Blogging App", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project selfDrivingCarProjectWebsite = new Project(UUID.randomUUID(), "Self Driving Car Project Website", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project clnGithubIoProject = new Project(UUID.randomUUID(), "CodyNicholson.github.io", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project portfolioWebsite = new Project(UUID.randomUUID(), "Portfolio Website", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project springBootKafkaProject = new Project(UUID.randomUUID(), "Spring Boot Kafka Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project fastStyleTransferProject = new Project(UUID.randomUUID(), "Fast Style Transfer Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project rixusProject = new Project(UUID.randomUUID(), "Rixus Project", "Voice operated personal assistant - maybe built using Flutter?", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project mindVaultProject = new Project(UUID.randomUUID(), "Mind Vault Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project stockScrapperProject = new Project(UUID.randomUUID(), "Stock Scrapper Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project tetrisProject = new Project(UUID.randomUUID(), "Tetris Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project enodoProject = new Project(UUID.randomUUID(), "Enodo Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project multiplayerTetrisProject = new Project(UUID.randomUUID(), "Multiplayer Tetris Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project memeCreatorProject = new Project(UUID.randomUUID(), "Meme Creator Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project starshotRicochetProject = new Project(UUID.randomUUID(), "Starshot Ricochet Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project findingLaneLinesProject = new Project(UUID.randomUUID(), "Finding Lane Lines Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project trafficSignClassifierProject = new Project(UUID.randomUUID(), "Traffic Sign Classifier Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project behavioralCloningProject = new Project(UUID.randomUUID(), "Behavioral Cloning Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project advancedLaneLineFindingProject = new Project(UUID.randomUUID(), "Advanced Lane Line Finding Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project vehicleDetectionProject = new Project(UUID.randomUUID(), "Vehicle Detection Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project extendedKalmanFilterProject = new Project(UUID.randomUUID(), "Extended Kalman Filter Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project unscentedKalmanFilter = new Project(UUID.randomUUID(), "Unscented Kalman Filter Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project kidnappedVehicleProject = new Project(UUID.randomUUID(), "Kidnapped Vehicle Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project pidControllerProject = new Project(UUID.randomUUID(), "PID Controller Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
+        Project modelPredictiveControllerProject = new Project(UUID.randomUUID(), "Model Predictive Controller Project", "description2", new Date(), null, "deployedLink2", "docLink2", "codeLink2");
 
-        projectRepository.save(project1);
-        projectRepository.save(project2);
+        projectRepository.save(projectTrackingProject);
+        projectRepository.save(issTrackingProject);
 
-        ProjectTask task1 = new ProjectTask(project1.getId(), "name 1", "description 1", "AC 1", 3.0, "Status 1", null);
-        ProjectTask task2 = new ProjectTask(project1.getId(), "name 2", "description 2", "AC 2", 1, "Status 2", null);
-        ProjectTask task3 = new ProjectTask(project2.getId(), "name 3", "description 3", "AC 3", 8, "Status 3", "blocked");
-        ProjectTask task4 = new ProjectTask(project2.getId(), "name 4", "description 4", "AC 4", 1, "Status 4", null);
-        ProjectTask task5 = new ProjectTask(project2.getId(), "name 5", "description 5", "AC 5", 2, "Status 5", "blocked");
+        ProjectTask task1 = new ProjectTask(projectTrackingProject.getId(), "Add Flyway DB Migrations", "description 1", "AC 1", 3.0, "Status 1", null);
+        ProjectTask task2 = new ProjectTask(projectTrackingProject.getId(), "Create New Project Selection Component", "description 2", "AC 2", 1, "Status 2", null);
+        ProjectTask task3 = new ProjectTask(projectTrackingProject.getId(), "Add JWT Token Authentication", "description 2", "AC 2", 1, "Status 2", null);
+        ProjectTask task4 = new ProjectTask(projectTrackingProject.getId(), "Add Export DB As JSON file", "description 2", "AC 2", 1, "Status 2", null);
+        ProjectTask task5 = new ProjectTask(issTrackingProject.getId(), "Add ISS Speed Graph", "description 4", "AC 4", 1, "Status 4", null);
+        ProjectTask task6 = new ProjectTask(issTrackingProject.getId(), "Add ISS Most Common Weather Condition Graph", "description 5", "AC 5", 2, "Status 5", "blocked");
+        ProjectTask task7 = new ProjectTask(issTrackingProject.getId(), "Add ISS Add Average Temperature", "description 3", "AC 3", 8, "Status 3", "blocked");
+        ProjectTask task8 = new ProjectTask(issTrackingProject.getId(), "Add ISS Most Visited Countries", "description 3", "AC 3", 8, "Status 3", "blocked");
+        ProjectTask task9 = new ProjectTask(issTrackingProject.getId(), "Add Export DB As JSON File", "description 3", "AC 3", 8, "Status 3", "blocked");
 
         projectTaskRepository.save(task1);
         projectTaskRepository.save(task2);
